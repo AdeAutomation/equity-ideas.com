c163e39eb14bd37cf58da36eb1d5c9c6656da48f
Ade <yemi4ade@yahoo.com>
2020-05-18 12:29:17 +0100

    Update stocks.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,7 @@
 
 
 
-/**
-INDEX PAGE - This function is used in the loop game to display multiplication table for input numbers and validate input fields
- **/
-function add() {
-	var x = parseInt(document.getElementById("x").value);
-	var y = parseInt(document.getElementById("y").value);
-	var xField = document.getElementById("x").value;
-	var yField = document.getElementById("y").value;
-	var totalOutput = "";
 
-	if (xField == "" || yField == "" || isNaN(xField) || isNaN(yField)) {
-		alert("Please enter a number in both fields");
-		return false;
-	}
-
-	var j = 1;
-	for (i = 1; i <= y; i = x * j) {
-		var c = i;
-		totalOutput = totalOutput + x + " * " + j + " = " + i + "<br>";
-		j = j + 1;
-	}
-	document.getElementById("demo").innerHTML = totalOutput;
-}
-
-/**
-This function gets that user enters username and password to login
- **/
-function validateLoginForm() {
-
-	var email = document.getElementById("loginemail").value;
-	var password = document.getElementById("password").value;
-	localStorage.setItem("storageName", email);
-	var limit = 20;
-
-	if (email == "" || password == "" || email.length > limit) {
-		alert("Please note: Username and/or password cannot be blank and username cannot be more than 20 characters");
-		return false;
-	}
-
-	return true;
-
-}
 
 /**
 This function validates that the password field on login page only accepts alphanumeric inputs
@@ -82,47 +41,8 @@ function loginPageLoad() {
 	}
 }
 
-/**
-This function validates that the expected login credentials are submitted
-**/
-function validateLoginCredentials() {
 
-	var c = "xyzabc";
-	var d = c.substring(1, 3);
-	console.debug("value of d is : " + d);
 
-	var url_string = window.location.href;
-	console.log("search: " + window.location.search);
-	console.log("search: " + window.location.search.substring(0));
-	console.log("search: " + window.location.search.substring(1));
-	var params = window.location.search.substring(1);
-	var fullUserName = params.split("&")[0];
-	var fullPassword = params.split("&")[1];
-	console.log("User: " + fullUserName);
-	console.log("password: " + fullPassword);
-	var userName = decodeURIComponent(fullUserName.split("=")[1]);
-	var password = fullPassword.split("=")[1];
-	console.log("User Name : " + userName);
-	console.log("Password : " + password);
-
-	if (userName == "testuser@email.com" && password == "password12") {
-
-		document.getElementById("loginemail").innerHTML = "Welcome, " + userName;
-
-		$(document.body).show();
-	} else {
-		window.location = "login.html?invalidCredentials=true";
-	}
-
-}
-
-/**
-This function gets the username of the user and displays it on the 'My Account' page
- **/
-function loggedInUser(email) {
-	document.getElementById("loggedinuser").innerHTML = "Welcome, " + localStorage.getItem("storageName");
-
-}
 
 /**
 This function validates registration form inputs
@@ -149,3 +69,26 @@ function validateForm() {
 
 }
 
+
+
+/**
+ This function validates that the expected login credentials are submitted
+ **/
+function myFunction() {
+	var input, filter, table, tr, td, i, txtValue;
+	input = document.getElementById("myInput");
+	filter = input.value.toUpperCase();
+	table = document.getElementById("myTable");
+	tr = table.getElementsByTagName("tr");
+	for (i = 0; i < tr.length; i++) {
+		td = tr[i].getElementsByTagName("td")[0];
+		if (td) {
+			txtValue = td.textContent || td.innerText;
+			if (txtValue.toUpperCase().indexOf(filter) > -1) {
+				tr[i].style.display = "";
+			} else {
+				tr[i].style.display = "none";
+			}
+		}
+	}
+}
